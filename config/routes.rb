9a5ba13b8fefc1d781a5c01268edf6f0079e673a
AyakoShirono <imnhe.yg@gmail.com>
2020-08-17 17:09:46 +0900

    Merge branch 'master' into excrement-b

--- config/routes.rb
+++ config/routes.rb
@@ -5,18 +5,18 @@ Rails.application.routes.draw do
   devise_scope :user do
     get 'profiles', to: 'users/registrations#new_profile'
     post 'profiles', to: 'users/registrations#create_profile'
+    get 'profiles', to: 'users/registrations#edit_profile'
   end
   root 'items#index'
-  # resources :items do
-  #   collection do
-  #     get 'get_category_children', defaults: { format: 'json' }
-  #     get 'get_category_grandchildren', defaults: { format: 'json' }
-  #   end
-  # end
+
   resources :items do
     collection do
       get 'get_category_children', defaults: { format: 'json' }
       get 'get_category_grandchildren', defaults: { format: 'json' }
     end
   end
+  resources :users, only: [:show, :edit, :update, :destroy] 
+  resources :profiles, only: [:edit, :update]
+  
+
 end
